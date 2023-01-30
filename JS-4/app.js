89e406b196f58c1485a3b26d83e29c8a7a299b57
Qaisar Abbas <122449496+qaisar786abbas@users.noreply.github.com>
2023-01-30 07:25:35 -0800

    added html and Js files

---
+++ JS-4/app.js
@@ -0,0 +1,48 @@
+//#####CHAPTER NO 4
+// *****VARIABLES NAME: LEGEL & ILLEGAL
+
+// Question-no 1
+// var num1Num2Num3;
+
+//Question-no 2
+//*** LEGAL
+//    1-var $doller;
+//    2-var num;
+//    3-var hello_world;
+//    4-var value;  
+//    5-var int;
+
+//******ILLEGAL NUMBER
+//  1-var 2num;   
+//  2-var hello world;  
+//  3-var myVar;..because it is js keyword  
+//  4-var alert;  
+//  5- use capital letter carefully
+
+
+//Question-no 3
+// var name = "Rules for naming Js variables";
+// document.write(name);
+// var line1 = "Variable names can only contain number $ _ for example $my _1stVariable";
+// document.write(line1);
+// var line2 = "variable must begin with a letter, $ or_ For example:$name,_name or name";
+// document.write(line2);
+// var line3 = "variables name are ase sensitive";
+// document.write(line3);
+// var line4 = "variables name should nat bo Js keywords";
+// document.write(line4);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
